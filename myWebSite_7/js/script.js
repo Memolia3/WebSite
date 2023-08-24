d8609074cdf1b6574fa876ae2629aefb09f2880f
Memolia <130337811+Memolia3@users.noreply.github.com>
2023-08-24 14:32:21 +0900

    Update script.js

--- myWebSite_7/js/script.js
+++ myWebSite_7/js/script.js
@@ -3,21 +3,16 @@ var opacityValue = 0;
 
 window.onload = function() {
     if(fade !== null) {
-        console.log('フェードインを実行します');
         setInterval(()=>{
             if(opacityValue <= 1){
                 changeOpacity();
             }
         }, 70);
-    }else {
-        console.log('フェードインを実行しません');
     }
 };
 
 function changeOpacity() {
     this.opacityValue += 0.1;
     fade.style.opacity = this.opacityValue;
-    console.log('opacity値を' + this.opacityValue + 'に変更');
 }
-
 
